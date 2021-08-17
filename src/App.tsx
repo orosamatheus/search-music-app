c9fe98d38927e043071ffa7b80680906530a3af2
Matheus Rosa <matheusrosa.la@gmail.com>
2021-08-17 01:28:22 -0300

    feat: context and redux providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import { Router } from "react-router-dom";
 import { Routes } from "./routes";
 
+import { HomeProvider } from "./context/home"
 import history from './history';
+
 import GlobalStyle from "./styles/GlobalStyle";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes/>
-      <GlobalStyle/>
-    </Router>
+    <HomeProvider>
+      <Router history={history}>
+        <Header />
+        <Routes />
+        <Footer />
+        <GlobalStyle />
+      </Router>
+    </HomeProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from "react-redux"
+import { store, persistor } from "./store/store"
+import { PersistGate } from "redux-persist/integration/react"
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate persistor={persistor} loading={null}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
